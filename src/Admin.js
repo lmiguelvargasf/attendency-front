547818e961334c081433889d016e28f7bb1cea57
M <m@mathsistor.com>
2020-04-14 19:35:17 -0500

    Improve side bar menu

--- src/Admin.js
+++ src/Admin.js
@@ -1,71 +1,38 @@
 import React from 'react'
 import { Layout, Menu, Breadcrumb } from 'antd'
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons'
+import { UserOutlined, LaptopOutlined, CalendarOutlined } from '@ant-design/icons'
 
 import styles from './Admin.module.sass'
 
-const { SubMenu } = Menu
 const { Header, Content, Sider } = Layout
 
 const Admin = () => {
   return (
-    <Layout style={{height: '100vh'}}>
+    <Layout style={{ height: '100vh' }}>
       <Header className='header'>
         <div className={styles.logo} />
-        <Menu theme='dark' mode='horizontal' style={{float: 'right'}}>
+        <Menu theme='dark' mode='horizontal' style={{ float: 'right' }}>
           <Menu.Item key='1'>Log Out</Menu.Item>
         </Menu>
       </Header>
       <Layout>
-        <Sider width={200} className={styles.siteLayoutBackground}>
+        <Sider width={200}>
           <Menu
-            mode='inline'
             defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
             style={{ height: '100%', borderRight: 0 }}
           >
-            <SubMenu
-              key='sub1'
-              title={
-                <span>
-                  <UserOutlined />
-                subnav 1
-                </span>
-              }
-            >
-              <Menu.Item key='1'>option1</Menu.Item>
-              <Menu.Item key='2'>option2</Menu.Item>
-              <Menu.Item key='3'>option3</Menu.Item>
-              <Menu.Item key='4'>option4</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key='sub2'
-              title={
-                <span>
-                  <LaptopOutlined />
-                subnav 2
-                </span>
-              }
-            >
-              <Menu.Item key='5'>option5</Menu.Item>
-              <Menu.Item key='6'>option6</Menu.Item>
-              <Menu.Item key='7'>option7</Menu.Item>
-              <Menu.Item key='8'>option8</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key='sub3'
-              title={
-                <span>
-                  <NotificationOutlined />
-                subnav 3
-                </span>
-              }
-            >
-              <Menu.Item key='9'>option9</Menu.Item>
-              <Menu.Item key='10'>option10</Menu.Item>
-              <Menu.Item key='11'>option11</Menu.Item>
-              <Menu.Item key='12'>option12</Menu.Item>
-            </SubMenu>
+            <Menu.Item key="1">
+              <UserOutlined />
+              <span>Projects</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <LaptopOutlined />
+              <span>Members</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <CalendarOutlined />
+              <span>Meetings</span>
+            </Menu.Item>
           </Menu>
         </Sider>
         <Layout style={{ padding: '0 24px 24px' }}>
@@ -75,14 +42,14 @@ const Admin = () => {
             <Breadcrumb.Item>App</Breadcrumb.Item>
           </Breadcrumb>
           <Content
-            className='site-layout-background'
+            className={styles.siteLayoutBackground}
             style={{
               padding: 24,
               margin: 0,
               minHeight: 280
             }}
           >
-          Content
+            Content
           </Content>
         </Layout>
       </Layout>
