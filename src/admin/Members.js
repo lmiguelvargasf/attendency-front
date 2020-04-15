68d73173f6170c2cdf7a75821d7b875595250f62
M <m@mathsistor.com>
2020-04-14 21:55:24 -0500

    Hard-code table for members

--- src/admin/Members.js
+++ src/admin/Members.js
@@ -1,7 +1,62 @@
 import React from 'react'
+import { Table, Space } from 'antd'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 const Members = () => {
-  return <h1>Members</h1>
+  const columns = [
+    {
+      title: 'Full Name',
+      dataIndex: 'fullName',
+      key: 'fullName',
+      render: fullName => <a>{fullName}</a>
+    },
+    {
+      title: 'Preferred Name',
+      dataIndex: 'preferredName',
+      key: 'fullName'
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email'
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <Space size='middle'>
+          <FontAwesomeIcon icon={faEdit} />
+          <FontAwesomeIcon icon={faTrash} />
+        </Space>
+      )
+    }
+  ]
+
+  const data = [
+    {
+      key: '1',
+      fullName: 'Martina W. Guzman',
+      preferredName: 'Winona',
+      email: 'martina.winona@gmail.com'
+    },
+    {
+      key: '2',
+      fullName: 'Isaac Newton',
+      preferredName: 'Isaac',
+      email: 'isaac.newton@gmail.com'
+    },
+    {
+      key: '3',
+      fullName: 'Nikola Tesla',
+      preferredName: 'Nikola',
+      email: 'nikola.tesla@gmail.com'
+    }
+  ]
+
+  return (
+    <Table columns={columns} dataSource={data} pagination={false} />
+  )
 }
 
 export default Members
