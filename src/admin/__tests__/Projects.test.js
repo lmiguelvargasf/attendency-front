2188d9de5276eae8b6cf8bc066e39bcf3072e2b0
M <m@mathsistor.com>
2020-04-18 22:09:04 -0500

    Add describe for Projects components

--- src/admin/__tests__/Projects.test.js
+++ src/admin/__tests__/Projects.test.js
@@ -2,8 +2,11 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import Projects from '../Projects'
 
-it('renders Projects component properly', () => {
-  const component = renderer.create(<Projects />)
-  const tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
+
+describe('Projects component', () => {
+  it('matches snapshot', () => {
+    const component = renderer.create(<Projects />)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
