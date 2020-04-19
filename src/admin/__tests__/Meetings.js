6c1bec7a7e79f0f2c4d1e65ef886e5c5688d43aa
M <m@mathsistor.com>
2020-04-19 13:05:04 -0500

    Implement testing for Meetings component

--- src/admin/__tests__/Meetings.js
+++ src/admin/__tests__/Meetings.js
@@ -1,9 +1,96 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import { render, cleanup } from '@testing-library/react'
 import Meetings from '../Meetings'
+import useAxios from 'axios-hooks'
+jest.mock('axios-hooks')
 
-it('renders Meetings component properly', () => {
-  const component = renderer.create(<Meetings />)
-  const tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
+const TABLE_TEST_ID = 'meeting-table'
+const fakeData = [
+  {
+    key: 1,
+    project: 'Testing Project',
+    date: '2020-04-18',
+    time: '18:00'
+  },
+  {
+    key: 2,
+    project: 'Another Testing Project',
+    date: '2020-04-18',
+    time: '18:00'
+  }
+]
+
+describe('Meetings component', () => {
+  afterEach(cleanup)
+
+  describe('loading data', () => {
+    beforeAll(() => {
+      useAxios.mockImplementation(url => [{
+        data: null,
+        loading: true,
+        error: null
+      }])
+    })
+
+    it('matches snapshot when loading data', () => {
+      const { asFragment } = render(<Meetings />)
+      expect(asFragment()).toMatchSnapshot()
+    })
+
+    it('renders loading', () => {
+      const { getByTestId, queryByTestId } = render(<Meetings />)
+      expect(queryByTestId(TABLE_TEST_ID)).toBeNull()
+      expect(getByTestId('loading')).not.toBeNull()
+      expect(queryByTestId('error')).toBeNull()
+    })
+  })
+
+  describe('error', () => {
+    beforeAll(() => {
+      useAxios.mockImplementation(url => [{
+        data: null,
+        loading: false,
+        error: 'Error'
+      }])
+    })
+
+    it('matches snapshot when having error', () => {
+      const { asFragment } = render(<Meetings />)
+      expect(asFragment()).toMatchSnapshot()
+    })
+
+    it('renders error', () => {
+      const { getByTestId, queryByTestId } = render(<Meetings />)
+      expect(queryByTestId(TABLE_TEST_ID)).toBeNull()
+      expect(queryByTestId('loading')).toBeNull()
+      expect(getByTestId('error')).not.toBeNull()
+    })
+  })
+
+  describe('meetings table', () => {
+    beforeAll(() => {
+      useAxios.mockImplementation(url => [{
+        data: fakeData,
+        loading: false,
+        error: null
+      }])
+    })
+
+    it('matches snapshop when displaying table', () => {
+      const { asFragment } = render(<Meetings />)
+      expect(asFragment()).toMatchSnapshot()
+    })
+
+    it('renders meetings table', async () => {
+      useAxios.mockImplementation(url => [{
+        data: fakeData,
+        loading: false,
+        error: null
+      }])
+      const { getByTestId, queryByTestId } = render(<Meetings />)
+      expect(getByTestId(TABLE_TEST_ID)).not.toBeNull()
+      expect(queryByTestId('loading')).toBeNull()
+      expect(queryByTestId('error')).toBeNull()
+    })
+  })
 })
