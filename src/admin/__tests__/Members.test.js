5f6529750001d3c966bf9589b0be604c833aa057
M <m@mathsistor.com>
2020-04-19 12:32:33 -0500

    Rename file

--- src/admin/__tests__/Members.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import Members from '../Members'
-
-it('renders Members component properly', () => {
-  const component = renderer.create(<Members />)
-  const tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
-})
