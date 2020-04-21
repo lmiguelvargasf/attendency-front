1dd6e7854de86a6f2d2848aed69dc56cfa929cef
M <m@mathsistor.com>
2020-04-20 22:47:26 -0500

    Update tests for RemoveObjectButton

--- src/admin/__tests__/RemoveObjectButton.js
+++ src/admin/__tests__/RemoveObjectButton.js
@@ -5,8 +5,7 @@ import useAxios from 'axios-hooks'
 jest.mock('axios-hooks')
 
 describe('RemoveObjectButton component', () => {
-  const object = { key: 1 }
-  const url = 'https://example.com/api'
+  const object = { key: 1, url: 'https://example.com/api' }
   const executeMock = jest.fn()
   const updateObjectsMock = jest.fn()
   let component
@@ -14,7 +13,7 @@ describe('RemoveObjectButton component', () => {
   beforeEach(() => {
     useAxios.mockReturnValue([{}, executeMock])
     component = (
-      <RemoveObjectButton url={url} object={object} updateObjects={updateObjectsMock} />
+      <RemoveObjectButton object={object} updateObjects={updateObjectsMock} />
     )
   })
 
@@ -27,7 +26,7 @@ describe('RemoveObjectButton component', () => {
 
   it('execute and updateObjectsMock are called when clicking on component', async () => {
     const { getByTestId } = render(component)
-    await fireEvent.click(getByTestId(url))
+    await fireEvent.click(getByTestId(object.url))
     expect(executeMock).toHaveBeenCalled()
     expect(updateObjectsMock).toHaveBeenCalledWith(object)
   })
