e0235e3445e9577ac06ebfed389e910a7a48385a
M <m@mathsistor.com>
2020-04-21 22:28:29 -0500

    Add basic implementation of form for project

--- src/admin/CreateProject.js
+++ src/admin/CreateProject.js
@@ -1,10 +1,62 @@
 import React from 'react'
+import { Form, Input, Button, DatePicker } from 'antd'
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 8 }
+}
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 8 }
+}
 
 const CreateProject = () => {
+  const onFinish = values => {
+    console.log('Success:', values)
+  }
+
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo)
+  }
+
   return (
-    <div>
-      Create project
-    </div>
+    <>
+      <h2>Create Project</h2>
+      <Form
+        {...layout}
+        name='basic'
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label='Title'
+          name='title'
+          rules={[{ required: true, message: "Please input your project's title!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label='Start date'
+          name='startDate'
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <DatePicker />
+        </Form.Item>
+        <Form.Item
+          label='Description'
+          name='description'
+        >
+          <Input.TextArea rows={4} />
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type='primary' htmlType='submit'>
+            Create
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   )
 }
 
