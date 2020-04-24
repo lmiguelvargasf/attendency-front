22ff2eccad665e58a6fa25f4d13b871f671bf3eb
M <m@mathsistor.com>
2020-04-23 22:40:04 -0500

    Replace logic with ProjectsTable component

--- src/admin/projects/Projects.js
+++ src/admin/projects/Projects.js
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
 import useAxios from 'axios-hooks'
-import { Table, Space, Button } from 'antd'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit, faCalendar, faUserPlus, faPlus } from '@fortawesome/free-solid-svg-icons'
-import RemoveObjectButton from '../RemoveObjectButton'
+import ProjectsTable from './ProjectsTable'
 
 const Projects = () => {
   const [projects, setProjects] = useState([])
@@ -25,65 +21,8 @@ const Projects = () => {
   if (loading) return <p data-testid='loading'>Loading...</p>
   if (error) return <p data-testid='error'>Error!</p>
 
-  const columns = [
-    {
-      title: 'Title',
-      dataIndex: 'title',
-      key: 'title',
-      render: title => <a>{title}</a>
-    },
-    {
-      title: 'Start Date',
-      dataIndex: 'startDate',
-      key: 'startDate'
-    },
-    {
-      title: 'Description',
-      dataIndex: 'description',
-      key: 'description',
-      render: description => {
-        if (description) {
-          return description.length < 50 ? description : `${description.substring(0, 50)}...`
-        }
-        return '-'
-      }
-    },
-    {
-      title: 'Team',
-      dataIndex: 'team',
-      key: 'team'
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      render: (text, record, index) => (
-        <Space size='middle'>
-          <FontAwesomeIcon icon={faEdit} />
-          <FontAwesomeIcon icon={faCalendar} />
-          <FontAwesomeIcon icon={faUserPlus} />
-          <RemoveObjectButton object={record} removeObject={updateProjects} />
-        </Space>
-      )
-    }
-  ]
-
   return (
-    <Space direction='vertical' size='middle' style={{ width: '100%' }}>
-      <Link to='/admin/projects/create'>
-        <Button type='primary'>
-          <Space>
-            <FontAwesomeIcon icon={faPlus} />
-            <span>New project</span>
-          </Space>
-        </Button>
-      </Link>
-      <Table
-        data-testid='project-table'
-        columns={columns}
-        dataSource={projects}
-        pagination={false}
-      />
-    </Space>
+    <ProjectsTable projects={projects} removeProject={updateProjects} />
   )
 }
 
