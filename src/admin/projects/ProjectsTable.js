53c7cb41696575e3f7880cb88f3a3d76a6d0c847
M <m@mathsistor.com>
2020-04-23 22:39:46 -0500

    Implement component ProjectsTable

---
+++ src/admin/projects/ProjectsTable.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Table, Space, Button } from 'antd'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit, faCalendar, faUserPlus, faPlus } from '@fortawesome/free-solid-svg-icons'
+import RemoveObjectButton from '../RemoveObjectButton'
+
+const ProjectsTable = ({ projects, removeProject }) => {
+  const columns = [
+    {
+      title: 'Title',
+      dataIndex: 'title',
+      key: 'title',
+      render: title => <a>{title}</a>
+    },
+    {
+      title: 'Start Date',
+      dataIndex: 'startDate',
+      key: 'startDate'
+    },
+    {
+      title: 'Description',
+      dataIndex: 'description',
+      key: 'description',
+      render: description => {
+        if (description) {
+          return description.length < 50 ? description : `${description.substring(0, 50)}...`
+        }
+        return '-'
+      }
+    },
+    {
+      title: 'Team',
+      dataIndex: 'team',
+      key: 'team'
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record, index) => (
+        <Space size='middle'>
+          <FontAwesomeIcon icon={faEdit} />
+          <FontAwesomeIcon icon={faCalendar} />
+          <FontAwesomeIcon icon={faUserPlus} />
+          <RemoveObjectButton object={record} removeObject={removeProject} />
+        </Space>
+      )
+    }
+  ]
+
+  return (
+    <Space direction='vertical' size='middle' style={{ width: '100%' }}>
+      <Link to='/admin/projects/create'>
+        <Button type='primary'>
+          <Space>
+            <FontAwesomeIcon icon={faPlus} />
+            <span>New project</span>
+          </Space>
+        </Button>
+      </Link>
+      <Table
+        data-testid='project-table'
+        columns={columns}
+        dataSource={projects}
+        pagination={false}
+      />
+    </Space>
+  )
+}
+
+export default ProjectsTable
