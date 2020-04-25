37b7ea6b2ac4b1553e7055a8540c4629e77dbbc4
M <m@mathsistor.com>
2020-04-24 22:59:43 -0500

    Implement component MembersTable

--- src/admin/members/Members.js
+++ src/admin/members/Members.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import useAxios from 'axios-hooks'
-import { Table, Space, Button } from 'antd'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit, faTrash, faPlus } from '@fortawesome/free-solid-svg-icons'
-import RemoveObjectButton from '../RemoveObjectButton'
+import MembersTable from './MembersTable'
 
 const Members = () => {
   const [members, setMembers] = useState([])
@@ -24,51 +21,8 @@ const Members = () => {
   if (loading) return <p data-testid='loading'>Loading...</p>
   if (error) return <p data-testid='error'>Error!</p>
 
-  const columns = [
-    {
-      title: 'First Name',
-      dataIndex: 'firstName',
-      key: 'firstName',
-      render: firstName => <a>{firstName}</a>
-    },
-    {
-      title: 'Last Name',
-      dataIndex: 'lastName',
-      key: 'lastName'
-    },
-    {
-      title: 'Email',
-      dataIndex: 'email',
-      key: 'email'
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      render: (text, record) => (
-        <Space size='middle'>
-          <FontAwesomeIcon icon={faEdit} />
-          <FontAwesomeIcon icon={faTrash} />
-          <RemoveObjectButton object={record} removeObject={updateMembers} />
-        </Space>
-      )
-    }
-  ]
-
   return (
-    <Space direction='vertical' size='middle' style={{ width: '100%' }}>
-      <Button type='primary'>
-        <Space>
-          <FontAwesomeIcon icon={faPlus} />
-          <span>New member</span>
-        </Space>
-      </Button>
-      <Table
-        data-testid='member-table'
-        columns={columns}
-        dataSource={members}
-        pagination={false}
-      />
-    </Space>
+    <MembersTable members={members} updateMembers={updateMembers} />
   )
 }
 
---
+++ src/admin/members/MembersTable.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Table, Space, Button } from 'antd'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit, faTrash, faPlus } from '@fortawesome/free-solid-svg-icons'
+import RemoveObjectButton from '../RemoveObjectButton'
+
+const MembersTable = ({ members, updateMembers }) => {
+  const columns = [
+    {
+      title: 'First Name',
+      dataIndex: 'firstName',
+      key: 'firstName',
+      render: firstName => <a>{firstName}</a>
+    },
+    {
+      title: 'Last Name',
+      dataIndex: 'lastName',
+      key: 'lastName'
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email'
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <Space size='middle'>
+          <FontAwesomeIcon icon={faEdit} />
+          <FontAwesomeIcon icon={faTrash} />
+          <RemoveObjectButton object={record} removeObject={updateMembers} />
+        </Space>
+      )
+    }
+  ]
+
+  return (
+    <Space direction='vertical' size='middle' style={{ width: '100%' }}>
+      <Button type='primary'>
+        <Space>
+          <FontAwesomeIcon icon={faPlus} />
+          <span>New member</span>
+        </Space>
+      </Button>
+      <Table
+        data-testid='member-table'
+        columns={columns}
+        dataSource={members}
+        pagination={false}
+      />
+    </Space>
+  )
+}
+
+export default MembersTable
