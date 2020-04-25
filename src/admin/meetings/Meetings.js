5b937e258504799ac06b979bf6870ba53f1d01bd
M <m@mathsistor.com>
2020-04-25 12:23:52 -0500

    Implement function to add a new meeting and pass it as a prop of CreateMeeting

--- src/admin/meetings/Meetings.js
+++ src/admin/meetings/Meetings.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import useAxios from 'axios-hooks'
 import MeetingsTable from './MeetingsTable'
+import CreateMeeting from './CreateMeeting'
 
 const Meetings = () => {
   const [meetings, setMeetings] = useState([])
@@ -18,13 +19,16 @@ const Meetings = () => {
   const removeMeeting = (meetingToDelete) => {
     setMeetings(() => meetings.filter(meeting => meeting.key !== meetingToDelete.key))
   }
+  const addMeeting = (meeting) => {
+    setMeetings(meetings => [meeting, ...meetings])
+  }
 
   if (loading) return <p data-testid='loading'>Loading...</p>
   if (error) return <p data-testid='error'>Error!</p>
 
   return (
     <Switch>
-      {/* <Route path='/admin/meetings/create' render={props => <CreateMember {...props} addMember={addMember} />} /> */}
+      <Route path='/admin/meetings/create' render={props => <CreateMeeting {...props} addMeeting={addMeeting} />} />
       <Route path='/admin/meetings' render={props => <MeetingsTable {...props} meetings={meetings} removeMeeting={removeMeeting} />} />
     </Switch>
   )
