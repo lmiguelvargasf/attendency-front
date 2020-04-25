363831eed720f3a97a4bdfd992e922685d9e9948
M <m@mathsistor.com>
2020-04-24 19:21:11 -0500

    Refactor testing including some mocking

--- src/admin/__tests__/CreateProject.js
+++ src/admin/__tests__/CreateProject.js
@@ -2,8 +2,25 @@ import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { render, cleanup } from '@testing-library/react'
 import CreateProject from '../projects/CreateProject'
+import useAxios from 'axios-hooks'
+jest.mock('axios-hooks')
+
+const BASE_API_URL = process.env.REACT_APP_API_URL
 
 describe('CreateProject component', () => {
+  const addProjectMock = jest.fn()
+  const createProjectMock = jest.fn().mockReturnValue({
+    data: {
+      key: 1,
+      url: `${BASE_API_URL}/projects/10`,
+      title: 'Testing Project Alpha',
+      startDate: '2020-04-18',
+      description: 'This is just for testing',
+      team: 'A, B, C'
+    }
+  })
+  let component
+
   beforeAll(() => {
     Object.defineProperty(window, 'matchMedia', {
       writable: true,
@@ -19,9 +36,16 @@ describe('CreateProject component', () => {
       }))
     })
   })
+
+  beforeEach(() => {
+    useAxios.mockReturnValue([{}, createProjectMock])
+    component = <Router><CreateProject /></Router>
+  })
+
   afterEach(cleanup)
+
   it('matches snapshot', () => {
-    const { asFragment } = render(<Router><CreateProject /></Router>)
+    const { asFragment } = render(component)
     expect(asFragment()).toMatchSnapshot()
   })
 })
