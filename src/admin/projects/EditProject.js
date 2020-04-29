d065dd23bc946a8aaee4f046225660f3ccd90a71
M <m@mathsistor.com>
2020-04-28 22:34:53 -0500

    Use ProjectForm component to not to dry

--- src/admin/projects/EditProject.js
+++ src/admin/projects/EditProject.js
@@ -1,16 +1,9 @@
 import React from 'react'
-import { Form, Input, Button, DatePicker, message } from 'antd'
+import { message } from 'antd'
 import { useHistory, useLocation } from 'react-router-dom'
 import useAxios from 'axios-hooks'
 import moment from 'moment'
-
-const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 8 }
-}
-const tailLayout = {
-  wrapperCol: { offset: 8, span: 8 }
-}
+import ProjectForm from './ProjectForm'
 
 const EditProject = ({ updateProjects }) => {
   const history = useHistory()
@@ -50,42 +43,7 @@ const EditProject = ({ updateProjects }) => {
   return (
     <>
       <h2>Edit Project</h2>
-      <Form
-        data-testid='create-project-form'
-        {...layout}
-        name='project'
-        initialValues={project}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-      >
-        <Form.Item
-          label='Title'
-          name='title'
-          rules={[{ required: true, message: 'Please input the title!' }]}
-        >
-          <Input data-testid='project-title-input' />
-        </Form.Item>
-
-        <Form.Item
-          label='Start date'
-          name='startDate'
-          rules={[{ required: true, message: 'Please input the start date!' }]}
-        >
-          <DatePicker data-testid='project-date-picker' />
-        </Form.Item>
-        <Form.Item
-          label='Description'
-          name='description'
-        >
-          <Input.TextArea rows={4} data-testid='project-description' />
-        </Form.Item>
-
-        <Form.Item {...tailLayout}>
-          <Button type='primary' htmlType='submit' data-testid='create-project-button'>
-            Save
-          </Button>
-        </Form.Item>
-      </Form>
+      <ProjectForm project={project} onFinish={onFinish} onFinishFailed={onFinishFailed} />
     </>
   )
 }
