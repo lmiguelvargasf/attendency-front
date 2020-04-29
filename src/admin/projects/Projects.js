887aec5b67f0c43f59cf875eef1259a74b8d7e79
M <m@mathsistor.com>
2020-04-29 06:55:07 -0500

    Use useReducer instead of useState

--- src/admin/projects/Projects.js
+++ src/admin/projects/Projects.js
@@ -1,27 +1,44 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import useAxios from 'axios-hooks'
 import ProjectsTable from './ProjectsTable'
 import CreateProject from './CreateProject'
 import EditProject from './EditProject'
 
+const projectReducer = (state, action) => {
+  switch (action.type) {
+    case 'LOAD_PROJECTS':
+      return action.projects
+    case 'REMOVE_PROJECT':
+      return state.filter(project => project.key !== action.project.key)
+    case 'ADD_PROJECT':
+      return [action.project, ...state]
+    case 'UPDATE_PROJECT':
+      state[action.index] = action.project
+      return state
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`)
+  }
+}
+
 const Projects = () => {
-  const [projects, setProjects] = useState([])
+  const [projects, dispatch] = useReducer(projectReducer, [])
   const [{ data, loading, error }] = useAxios(
     `${process.env.REACT_APP_API_URL}/projects`
   )
 
-  useEffect(() => { setProjects(data) }, [data])
+  useEffect(() => {
+    dispatch({ type: 'LOAD_PROJECTS', projects: data })
+  }, [data])
 
-  const removeProject = (projectToDelete) => {
-    setProjects(() => projects.filter(project => project.key !== projectToDelete.key))
+  const removeProject = (project) => {
+    dispatch({ type: 'REMOVE_PROJECT', project })
   }
   const addProject = (project) => {
-    setProjects(projects => [project, ...projects])
+    dispatch({ type: 'ADD_PROJECT', project })
   }
-  const updateProjects = (updatedProject, index) => {
-    projects[index] = updatedProject
-    setProjects(projects)
+  const updateProjects = (project, index) => {
+    dispatch({ type: 'UPDATE_PROJECT', project, index })
   }
 
   if (loading) return <p data-testid='loading'>Loading...</p>
