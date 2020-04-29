d55a1f697616906d1853d3500ea3e4a72cb92881
M <m@mathsistor.com>
2020-04-29 06:43:50 -0500

    Optimize imports

--- src/admin/members/EditMember.js
+++ src/admin/members/EditMember.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useHistory, useLocation } from 'react-router-dom'
 import { Form, Input, Button, message } from 'antd'
 import useAxios from 'axios-hooks'
