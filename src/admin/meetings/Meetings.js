3fc52b151b261aed2ef22513e8a6249feec35f7c
M <m@mathsistor.com>
2020-04-30 00:04:20 -0500

    Use useReducer instead of useState

--- src/admin/meetings/Meetings.js
+++ src/admin/meetings/Meetings.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import useAxios from 'axios-hooks'
 import MeetingsTable from './MeetingsTable'
@@ -6,23 +6,38 @@ import CreateMeeting from './CreateMeeting'
 import EditMeeting from './EditMeeting'
 import Participation from './Participation'
 
+const meetingReducer = (state, action) => {
+  switch (action.type) {
+    case 'LOAD':
+      return action.meetings
+    case 'REMOVE':
+      return state.filter(meeting => meeting.key !== action.meeting.key)
+    case 'ADD':
+      return [action.meeting, ...state]
+    case 'UPDATE':
+      state[action.index] = action.meeting
+      return state
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`)
+  }
+}
+
 const Meetings = () => {
-  const [meetings, setMeetings] = useState([])
+  const [meetings, dispatch] = useReducer(meetingReducer, [])
   const [{ data, loading, error }] = useAxios(
     `${process.env.REACT_APP_API_URL}/meeting-table/`
   )
 
-  useEffect(() => { setMeetings(data) }, [data])
+  useEffect(() => { dispatch({ type: 'LOAD', meetings: data }) }, [data])
 
-  const removeMeeting = meetingToDelete => {
-    setMeetings(() => meetings.filter(meeting => meeting.key !== meetingToDelete.key))
+  const removeMeeting = meeting => {
+    dispatch({ type: 'REMOVE', meeting })
   }
   const addMeeting = meeting => {
-    setMeetings(meetings => [meeting, ...meetings])
+    dispatch({ type: 'ADD', meeting })
   }
-  const updateMeetings = (updatedMeeting, index) => {
-    meetings[index] = updatedMeeting
-    setMeetings(meetings)
+  const updateMeetings = (meeting, index) => {
+    dispatch({ type: 'UPDATE', meeting, index })
   }
 
   if (loading) return <p data-testid='loading'>Loading...</p>
