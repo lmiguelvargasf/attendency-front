d9dcb3cc7d91d3e75886386b330e295d69f0bb05
M <m@mathsistor.com>
2020-04-29 22:05:25 -0500

    Simplify way of passing handlers

--- src/admin/members/Members.js
+++ src/admin/members/Members.js
@@ -23,30 +23,26 @@ const memberReducer = (state, action) => {
 
 const Members = () => {
   const [members, dispatch] = useReducer(memberReducer, [])
-  const [{ data, loading, error }] = useAxios(
-    `${process.env.REACT_APP_API_URL}/members/`
-  )
-
+  const [{ data, loading, error }] = useAxios(`${process.env.REACT_APP_API_URL}/members/`)
   useEffect(() => { dispatch({ type: 'LOAD', members: data }) }, [data])
-
-  const removeMember = (member) => {
-    dispatch({ type: 'REMOVE', member })
-  }
-  const addMember = (member) => {
-    dispatch({ type: 'ADD', member })
-  }
-  const updateMembers = (member, index) => {
-    dispatch({ type: 'UPDATE', member, index })
-  }
 
   if (loading) return <p data-testid='loading'>Loading...</p>
   if (error) return <p data-testid='error'>Error!</p>
 
   return (
     <Switch>
-      <Route path='/admin/members/create' render={props => <CreateMember {...props} addMember={addMember} />} />
-      <Route path='/admin/members/:key/edit' render={props => <EditMeeting {...props} updateMembers={updateMembers} />} />
-      <Route path='/admin/members' render={props => <MembersTable {...props} members={members} removeMember={removeMember} />} />
+      <Route
+        path='/admin/members/create'
+        render={props => <CreateMember {...props} addMember={member => dispatch({ type: 'ADD', member })} />}
+      />
+      <Route
+        path='/admin/members/:key/edit'
+        render={props => <EditMeeting {...props} updateMembers={(member, index) => dispatch({ type: 'UPDATE', member, index })} />}
+      />
+      <Route
+        path='/admin/members'
+        render={props => <MembersTable {...props} members={members} removeMember={member => dispatch({ type: 'REMOVE', member })} />}
+      />
     </Switch>
   )
 }
