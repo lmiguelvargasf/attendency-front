cb49e33eb664f60e82f566c754d7c9f0547e4ce1
M <m@mathsistor.com>
2020-05-02 14:08:44 -0500

    Optimize imports

--- src/admin/projects/ProjectsTable.js
+++ src/admin/projects/ProjectsTable.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect } from 'react'
+import React, { useReducer } from 'react'
 import { Link } from 'react-router-dom'
 import { Table, Space, Button, Modal, Select, message } from 'antd'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
