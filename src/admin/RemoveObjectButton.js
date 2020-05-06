87d3af2255719158c2556fa27fe1e4a42b0f681a
M <m@mathsistor.com>
2020-05-05 23:35:04 -0500

    Add prop-types for RemoveObjectButton

--- src/admin/RemoveObjectButton.js
+++ src/admin/RemoveObjectButton.js
@@ -3,6 +3,7 @@ import useAxios from 'axios-hooks'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import styles from './Admin.module.sass'
+import PropTypes from 'prop-types'
 
 const RemoveObjectButton = ({ object, removeObject }) => {
   const [, execute] = useAxios(
@@ -18,6 +19,11 @@ const RemoveObjectButton = ({ object, removeObject }) => {
     <button type='button' className={styles.linkButton} data-testid={object.url} onClick={remove}>
       <FontAwesomeIcon icon={faTrash} style={{ color: '#f2463d' }} />
     </button>)
+}
+
+RemoveObjectButton.propTypes = {
+  object: PropTypes.object,
+  removeObject: PropTypes.func
 }
 
 export default RemoveObjectButton
