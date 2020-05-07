fa24166b8ffaa11901d9789b3c5ac94fdbb23d20
M <m@mathsistor.com>
2020-05-07 01:25:09 -0500

    Fix broken tests

--- src/admin/__tests__/members/MembersTable.js
+++ src/admin/__tests__/members/MembersTable.js
@@ -3,6 +3,20 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { render, cleanup } from '@testing-library/react'
 import MembersTable from '../../members/MembersTable'
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn()
+  }))
+})
+
 describe('MembersTable component', () => {
   afterEach(cleanup)
 
--- src/admin/__tests__/projects/ProjectsTable.js
+++ src/admin/__tests__/projects/ProjectsTable.js
@@ -3,6 +3,20 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { render, cleanup } from '@testing-library/react'
 import ProjectsTable from '../../projects/ProjectsTable'
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn()
+  }))
+})
+
 describe('ProjectsTable component', () => {
   afterEach(cleanup)
 
