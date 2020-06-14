ddd3934277bdc033bcc00121f6f71ed88c60fbb5
M <m@mathsistor.com>
2020-06-14 14:26:20 -0500

    Fix navigation bar selected item

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Route, Switch, Link, Redirect, useLocation, useHistory } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Route, Switch, Redirect, useLocation, useHistory } from 'react-router-dom'
 import { Layout, Menu, Space } from 'antd'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDraftingCompass, faUsers, faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
@@ -14,19 +14,22 @@ const { Header, Content, Sider } = Layout
 
 const menuItems = [
   {
+    key: '1',
     icon: faDraftingCompass,
     label: 'Projects',
-    key: 'projects'
+    path: '/admin/projects'
   },
   {
+    key: '2',
     icon: faUsers,
     label: 'Members',
-    key: 'members'
+    path: '/admin/members'
   },
   {
+    key: '3',
     icon: faCalendarAlt,
     label: 'Meetings',
-    key: 'meetings'
+    path: '/admin/meetings'
   }
 ]
 
@@ -34,10 +37,21 @@ const Admin = () => {
   const location = useLocation()
   const history = useHistory()
   const [selectedMenuItem, setselectedMenuItem] = useState(location.pathname.replace('/admin/', ''))
+  const computeSelected = () => {
+    const selected = menuItems.find(_item => location.pathname.startsWith(_item.path))
+    return selected ? selected.key : null
+  }
+  useEffect(() => {
+    setselectedMenuItem(computeSelected())
+  }, [location])
   const logOut = () => {
     window.localStorage.removeItem('token')
     window.localStorage.removeItem('refreshToken')
     history.push('/login')
+  }
+  const onClickMenu = (item) => {
+    const clicked = menuItems.find(_item => _item.key === item.key)
+    history.push(clicked.path)
   }
 
   return (
@@ -55,13 +69,11 @@ const Admin = () => {
             style={{ height: '100%', borderRight: 0 }}
           >
             {menuItems.map(item => (
-              <Menu.Item key={item.key}>
-                <Link to={`/admin/${item.key}`} onClick={() => { setselectedMenuItem(item.key) }}>
-                  <Space size='middle'>
-                    <FontAwesomeIcon icon={item.icon} />
-                    <span>{item.label}</span>
-                  </Space>
-                </Link>
+              <Menu.Item key={item.key} onClick={onClickMenu}>
+                <Space size='middle'>
+                  <FontAwesomeIcon icon={item.icon} />
+                  <span>{item.label}</span>
+                </Space>
               </Menu.Item>
             ))}
           </Menu>
